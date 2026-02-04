427a5d24951a29d3ac903d0121f66901a871335d
Nonhlanhla Mokoena <mokoena3586@gmail.com>
2026-02-04 18:26:05 +0200

    Modify online book store

--- Intro_to_DB/alx_book_store.sql
+++ Intro_to_DB/alx_book_store.sql
@@ -1,39 +1,47 @@
-CREATE DATABASE alx_book_store;
+-- CREATE DATABASE
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
-'''Books table'''
-CREATE TABLE Books (
-    book_id PRIMARY KEY,
-    title VARCHAR(130),
-    author_id FOREIGN KEY,
-    price DOUBLE,
-    publication_date DATE,
-);
+-- USE DATABASE
+USE alx_book_store;
 
-'''Authors table'''
+-- AUTHORS TABLE
 CREATE TABLE Authors (
-    author_id PRIMARY KEY,
-    author_name VARCHAR(215)
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
 );
 
-'''Customers table'''
+-- BOOKS TABLE
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- CUSTOMERS TABLE
 CREATE TABLE Customers (
-    customer_id PRIMARY KEY,
-    customer_name VARCHAR(215),
-    email VARCHAR(215),
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
     address TEXT
 );
 
-'''Orders table'''
+-- ORDERS TABLE
 CREATE TABLE Orders (
-    order_id PRIMARY KEY,
-    customer_id FOREIGN KEY,
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
     order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-'''Order_details table'''
+-- ORDER_DETAILS TABLE
 CREATE TABLE Order_Details (
-    orderdetailid PRIMARY KEY,
-    order_id INT FOREIGN KEY,
-    book_id FOREIGN KEY,
-    quantity DOUBLE,
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
